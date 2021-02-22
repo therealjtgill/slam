Fixed bug in EKF-SLAM that caused the landmark-landmark portion of the covariance matrix to be full of 10000, instead of 10000 along the landmark-landmark diagonal.

ekf_slam.py
@@ -27,7 +27,6 @@ class EKFSlam:
             [0, 0, 0, 0, 0, 1]
          ]
       )
-
       self.F_map = np.eye(self.state_size)
       self.F_map[0:6, 0:6] = F
 
@@ -56,10 +55,11 @@ class EKFSlam:
          dtype=np.float32
       )
 
+      # self.x = np.random.rand(self.state_size
       self.x = np.zeros(self.state_size, dtype=np.float32)
       self.P = 0.3*np.eye(self.state_size, dtype=np.float32)
       # Set uncertainties of landmark positions to big-ish values.
-      self.P[6:, 6:] += 10000
+      self.P[6:, 6:] = 10000*np.eye(self.state_size - 6)
       self.K = np.eye(self.state_size, dtype=np.float32)
 
    def propagateState(self, accel_input):
@@ -83,7 +83,7 @@ class EKFSlam:
       E.g. landmark 0's features appears at index ID 6, 7 in the state vector.
       '''
       self.H_map[:, :] = 0.0
-      meas_vec = np.zeros(self.state_size)
+      meas_vec = np.zeros(self.state_size, dtype=np.float32)
       # Change observation matrix and observation vector based on the
       # availability of measurements.
       if pos_meas is not None:
@@ -101,10 +101,14 @@ class EKFSlam:
                (6 + 2*lid):(6 + 2*lid + 2),
                (6 + 2*lid):(6 + 2*lid + 2)
             ] = np.eye(2)
+            self.H_map[6 + 2*lid, 0] = -1
+            self.H_map[6 + 2*lid + 1, 1] = -1
       x_minus = self.propagateState(accel_input)
       P_minus = np.dot(np.dot(self.F_map, self.P), self.F_map.T) + self.Q_proc
       gain_fac = np.linalg.inv(np.dot(np.dot(self.H_map, P_minus), self.H_map.T) + self.R_meas)
       self.K = np.dot(np.dot(P_minus, self.H_map.T), gain_fac)
-      self.x = x_minus + np.dot(self.K, (meas_vec - np.dot(self.H_map, x_minus)))
+      innovations = meas_vec - np.dot(self.H_map, x_minus)
+      # print("innovations:", innovations, "\n", meas_vec)
+      self.x = x_minus + np.dot(self.K, innovations)
       self.P = np.dot(np.eye(self.state_size) - np.dot(self.K, self.H_map), P_minus)
       return self.x

range_bearing_sensor.py
@@ -17,7 +17,9 @@ class RangeBearingSensor:
 
    def getMeasurements(self, true_sensor_pos, true_landmarks):
       meas_landmarks = {
-         i: np.random.multivariate_normal(l, self.Q_meas)
+         # We actually measure the position of the landmark relative to our
+         # true global position.
+         i: np.random.multivariate_normal(l, self.Q_meas) - true_sensor_pos
          for i, l in enumerate(true_landmarks)
          if ((np.linalg.norm(true_sensor_pos - l) <= self.R_max) and 
             (np.linalg.norm(true_sensor_pos - l) >= self.R_min))

viz.py
@@ -101,7 +101,7 @@ def main():
     vehicle_viz = DotViz((0, 0, 255), (0, 0), screen_converter)
     vehicle_dynamics = VehicleDynamics([0.0, 0.0], [0.0, 0.0], drag, mass)
     vehicle_controller = VehicleController(1, 0.001, 0.1, -5, 5)
-    vehicle_controller.setWaypoint([30, 10])
+    vehicle_controller.setWaypoint([-30, -10])
 
     landmark_vizs = [
         DotViz(
@@ -121,8 +121,10 @@ def main():
     R_map[0:6, 0:6] = 0.4*np.eye(6)
     R_map[6:, 6:] = 0.5*np.eye(2*num_landmarks)
     ekf_slam = EKFSlam(mass, drag, Q_map, R_map, num_landmarks)
-    landmark_sensor = RangeBearingSensor(0.5, 150, 0.9*np.eye(2))
+    landmark_sensor = RangeBearingSensor(0.5, 50, 0.9*np.eye(2))
 
+    pos_hat = np.zeros(2)
+    vel_hat = np.zeros(2)
     running = True
     counter = 0
     while running:
@@ -132,34 +134,30 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        accel_cmd = vehicle_controller.pursueWaypoint(vehicle_dynamics.pos, vehicle_dynamics.vel)
+        accel_cmd = vehicle_controller.pursueWaypoint(pos_hat, vel_hat)
         vehicle_dynamics.update(accel_cmd)
-        force = vehicle_dynamics.force/vehicle_dynamics.mass - accel_cmd
-        if counter % 1000 == 0:
+        force = vehicle_dynamics.force/vehicle_dynamics.mass# - accel_cmd
+        if counter % 1000 == 0 and False:
             pos_meas = np.random.multivariate_normal(vehicle_dynamics.pos, 0.3*np.eye(2))
         else:
             pos_meas = None
-        if counter % 1 == 0:
+        if counter % 10 == 0:
             landmark_meas = landmark_sensor.getMeasurements(
                 vehicle_dynamics.pos,
                 [l.pos for l in landmark_vizs]
             )
-            # print(landmark_meas)
-            # print([l.pos for l in landmark_vizs])
-            # sys.exit()
         else:
             landmark_meas = None
         accel_meas = accelerometer.getMeasurements(force)
         predicted_map_and_state = ekf_slam.update(accel_cmd, pos_meas, accel_meas, landmark_meas)
-        # print("accel meas:", accel_meas)
-        predicted_state = kf.update(accel_cmd, pos_meas, accel_meas)
+        # predicted_state = kf.update(accel_cmd, pos_meas, accel_meas)
         pos_hat = predicted_map_and_state[0:2]
+        vel_hat = predicted_map_and_state[2:4]
         # print(predicted_map_and_state[6:])
         pos_cov = ekf_slam.P[0:2, 0:2]
         # vel_cov = kf.P[2:4, 2:4]
         # acc_cov = kf.P[4:6, 4:6]
         print(pos_cov)
-        # print(counter*0.001)
         screen_pos_hat = screen_converter.convertWorldToScreen(*pos_hat)
         pygame.draw.circle(screen, (255, 0, 0), screen_pos_hat, 6, 1)
         vehicle_viz.updateWorldPosition(vehicle_dynamics.pos)