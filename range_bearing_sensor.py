149c6ec1f842e6fdfee401ac462fd479e226f0ba
Jonathan Gill <jtgill@email.arizona.edu>
2021-02-22 00:15:47 -0800

    EKF-SLAM (#2)
    * Added new implementation of EKF that extends to SLAM.
    
    * Fixed a few bugs. Added display of dots for landmarks. Swapped EKF for EKF-SLAM alg in main loop.
    
    * Fixed bug in EKF-SLAM that caused the landmark-landmark portion of the covariance matrix to be full of 10000, instead of 10000 along the landmark-landmark diagonal.

--- range_bearing_sensor.py
+++ range_bearing_sensor.py
@@ -13,13 +13,15 @@ class RangeBearingSensor:
    ):
       self.R_min = min_range
       self.R_max = max_range
-      self.num_angular_samples = num_angular_samples
-      self.Q_meas = measurement_covariance
+      self.Q_meas = np.array(measurement_covariance)
 
    def getMeasurements(self, true_sensor_pos, true_landmarks):
-      meas_landmarks = [
-         np.random.normal(l, self.Q_meas) for l in true_landmarks
+      meas_landmarks = {
+         # We actually measure the position of the landmark relative to our
+         # true global position.
+         i: np.random.multivariate_normal(l, self.Q_meas) - true_sensor_pos
+         for i, l in enumerate(true_landmarks)
          if ((np.linalg.norm(true_sensor_pos - l) <= self.R_max) and 
             (np.linalg.norm(true_sensor_pos - l) >= self.R_min))
-      ]
+      }
       return meas_landmarks
