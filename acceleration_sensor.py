a594e040515997cfab3ab244691215f475b197d0
therealjtgill <jtgill@email.arizona.edu>
2021-02-21 16:19:31 -0800

    Acceleration sensor update.

--- acceleration_sensor.py
+++ acceleration_sensor.py
@@ -2,4 +2,7 @@ import numpy as np
 
 class AccelerationSensor:
    def __init__(self, measurement_covariance):
-      
+      self.R_meas = measurement_covariance
+
+   def getMeasurements(self, true_accel):
+      return np.random.normal(true_accel, self.R_meas)
